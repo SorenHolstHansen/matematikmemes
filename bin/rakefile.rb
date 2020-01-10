de84de7aaec37337daff8b0c800553ea9bb66073
SorenHolstHansen <soerenhh@pm.me>
2020-01-10 12:41:55 +0100

    ...

--- bin/rakefile.rb
+++ bin/rakefile.rb
@@ -1,5 +1,5 @@
 desc "This task scrapes the matematik memes facebook page and updates the database"
-task :update_database => :environment do
+task :update_database do
     puts "Updating database..."
     update_database()
     puts "done."
--- config/boot.rb
+++ config/boot.rb
@@ -10,6 +10,7 @@ Dir.glob('./app/helpers/*.rb').each { |file| require file }
 require './app/app'
 Dir.glob('./app/models/*.rb').each { |file| require file }
 Dir.glob('./lib/*.rb').each { |file| require file }
+Dir.glob('./bin/*.rb').each { |file| require file }
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
