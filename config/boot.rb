4939e76d20cd750a5d53eb7793765db103d50261
SorenHolstHansen <soerenhh@pm.me>
2020-01-10 12:47:19 +0100

    Rake cron

--- config/boot.rb
+++ config/boot.rb
@@ -10,7 +10,6 @@ Dir.glob('./app/helpers/*.rb').each { |file| require file }
 require './app/app'
 Dir.glob('./app/models/*.rb').each { |file| require file }
 Dir.glob('./lib/*.rb').each { |file| require file }
-Dir.glob('./bin/*.rb').each { |file| require file }
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,8 @@
+require './config/boot'
+
 desc "This task scrapes the matematik memes facebook page and updates the database"
 task :update_database do
     puts "Updating database..."
     update_database()
-    puts "done."
+    puts "\ndone."
 end
